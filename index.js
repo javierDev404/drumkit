92a5eb3ee69f9cbb7463ecb20be6ee1dd90334a1
javierDev404 <78935955+javierDev404@users.noreply.github.com>
2021-09-09 22:19:45 -0700

    Bootcamp Class project files

---
+++ index.js
@@ -0,0 +1,62 @@
+var numberOfDrumButton = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButton; i++) {
+
+	//Detect button press
+	document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+		var buttonInnerHTML = this.innerHTML;
+		makeSound(buttonInnerHTML);
+		btnAnimation(buttonInnerHTML);
+
+	});
+}
+//detect keyboard press
+document.addEventListener("keydown", function(event) {
+	makeSound(event.key);
+	btnAnimation(event.key);
+})
+
+function makeSound(key) {
+	switch (key) {
+		case "w":
+			var tom1 = new Audio('./sounds/tom-1.mp3');
+			tom1.play();
+			break;
+		case "a":
+			var tom2 = new Audio('./sounds/tom-2.mp3');
+			tom2.play();
+			break;
+		case "s":
+			var tom3 = new Audio('./sounds/tom-3.mp3');
+			tom3.play();
+			break;
+		case "d":
+			var tom4 = new Audio('./sounds/tom-4.mp3');
+			tom4.play();
+			break;
+		case "j":
+			var crash = new Audio('./sounds/crash.mp3');
+			crash.play();
+			break;
+		case "k":
+			var kick = new Audio('./sounds/kick-bass.mp3');
+			kick.play();
+			break;
+		case "l":
+			var snare = new Audio('./sounds/snare.mp3');
+			snare.play();
+			break;
+		default:
+			consol.log(buttonInnerHTML);
+
+	}
+}
+
+function btnAnimation(currentKey) {
+	var activeBtn = document.querySelector("." + currentKey);
+	activeBtn.classList.add("pressed");
+	setTimeout(function(){
+		activeBtn.classList.remove("pressed");
+	},100)
+	//activeBtn.classList.remove("pressed");
+}
